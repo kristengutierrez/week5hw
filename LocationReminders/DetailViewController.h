25e62c7a50a51cefe50950a213500accc1c4b2f5
kristenkozmary <kristenkozmary@yahoo.com>
2015-09-02 15:43:18 -0700

    added detail view controller

---
+++ LocationReminders/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  LocationReminders
+//
+//  Created by Kristen Kozmary on 9/2/15.
+//  Copyright (c) 2015 koz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
